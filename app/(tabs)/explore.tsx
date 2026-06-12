95d854b400dddf3801442eb6b32294a0060ef1a0
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2026-06-12 12:08:41 +0530

    explore screen + a bit of notification screen

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,22 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-type Props = {}
+import { Colors } from "@/constants/Colors";
+import { CategoryType } from "@/types/type";
+import { useHeaderHeight } from "@react-navigation/elements";
+import axios from "axios";
+import { Image } from "expo-image";
+import { Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+type Props = {};
 
 const ExploreScreen = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Explore Screen</Text>
-    </View>
-  )
-}
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const headerHeight = useHeaderHeight();
+  useEffect(() => {
+    getCategories();
+  }, []);
 
-export default ExploreScreen
+  const getCategories = async () => {
+    const URL = "http://192.168.0.92:8000/categories";
+    const response = await axios.get(URL);
+    console.log(response.data);
+    setCategories(response.data);
+  };
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
+      <View style={[styles.container, { marginTop: headerHeight }]}>
+        <Text>Explore Screen</Text>
+        <FlatList
+          data={categories}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item, index }) => (
+            <view style={styles.itemWrapper}>
+              <Text style={styles.itemTitle}>{item.name}</Text>
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 100, height: 100, borderRadius: 10 }}
+              />
+            </view>
+          )}
+        />
+      </View>
+    </>
+  );
+};
+
+export default ExploreScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    paddingHorizontal: 20,
+  },
+  itemWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: Colors.extraLightGray,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  itemTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.black,
+  },
+});
--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,22 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-type Props = {}
+import { NotificationType } from "@/types/type";
+import { useHeaderHeight } from "@react-navigation/elements";
+import axios from "axios";
+import { Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+type Props = {};
 
 const NotificationsScreen = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Notifications Screen</Text>
-    </View>
-  )
-}
+  const [notification, setNotifications] = useState<NotificationType[]>([]);
+  const headerHeight = useHeaderHeight();
 
-export default NotificationsScreen
+  useEffect(() => {
+    getNotifications();
+  }, []);
+
+  const getNotifications = async () => {
+    const URL = "http://192.168.0.92:8000/notifcations";
+    const response = await axios.get(URL);
+    console.log(response.data);
+    setNotifications(response.data);
+  };
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
+
+      <View style={[styles.container, { marginTop: headerHeight }]}>
+        <Text>Notifications Screen</Text>
+      </View>
+    </>
+  );
+};
+
+export default NotificationsScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    paddingHorizontal: 20,
+  },
+});
